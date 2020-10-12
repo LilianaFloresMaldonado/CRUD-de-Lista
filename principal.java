ae30331b1b3862ec05ab9676c09ea4accc8cc2b9
LilianaFloresMaldonado <71808888+LilianaFloresMaldonado@users.noreply.github.com>
2020-10-12 11:47:36 -0500

    Create principal.java

---
+++ principal.java
@@ -0,0 +1,21 @@
+package lista;
+
+
+public class principal {
+   private String nombre;
+
+    public principal(){}
+ 
+    public principal(String nombre ){
+        this.nombre = nombre;       
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+} 
